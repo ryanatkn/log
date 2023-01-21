a3457c3b34b6a72ef1bf8272702aff343f7a42db
Ryan Atkinson <mail@ryanatkn.com>
2023-01-20 20:30:44 -0700

    deployment

--- _app/immutable/chunks/1-f3a6bcca.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-079f96df.js";export{t as component};
---
+++ _app/immutable/chunks/2-c584bf6f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-584b6b04.js";export{t as component};
